b27b149ba2103eda6d6913f4d2138a4049b506e9
Tomas Ambrogi <tomi.ambro94@gmail.com>
2023-04-07 16:57:05 -0300

    feat: fixing format

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ terraform {
 }
 
 provider "aws" {
-  region              = "us-west-2"
+  region = "us-west-2"
   # shared_config_files = ["~/.aws/config"]
-  profile             = "dev-profile"
+  profile = "dev-profile"
 }
 
 module "compute" {
